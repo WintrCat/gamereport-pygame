c985d07a4412fd9ada5519b7ba6db0a064f90256
WintrCat <wintrcat.wilson@gmail.com>
2023-04-29 17:16:48 +0100

    Fixed brilliant algorithm bug
    Fixed a small bug in the brilliant move algorithm where a brilliant move would sometimes not be given if the sacrificed piece was not the one that just moved.

--- src/piece.py
+++ src/piece.py
@@ -10,12 +10,15 @@ pieceValues = {
 }
 
 # determines and returns whether a piece on a board is safe (defended / traded) or unsafe (hanging)
-def is_piece_safe(board: chess.Board, square: chess.Square) -> bool:
+def is_piece_safe(board: chess.Board, square: chess.Square, debug = False) -> bool:
     piece = board.piece_at(square)
     if piece.piece_type == chess.PAWN: return True
 
     attackers = board.attackers(not piece.color, square)
     defenders = board.attackers(piece.color, square)
+    if debug:
+        print("Attackers: " + str(len(attackers)))
+        print("Defenders: " + str(len(defenders)))
 
     # remove king as an attacker if there are any defenders
     if len(defenders) > 0:
@@ -25,7 +28,9 @@ def is_piece_safe(board: chess.Board, square: chess.Square) -> bool:
                 break
 
     # if there are no attackers on the piece, it is defended
-    if len(attackers) == 0: return True
+    if len(attackers) == 0:
+        if debug: print("No attackers, so is defended")
+        return True
 
     # if there are no legal captures of this piece, it is defended
     legallyCapturable = False
@@ -33,13 +38,20 @@ def is_piece_safe(board: chess.Board, square: chess.Square) -> bool:
         if move.to_square == square:
             legallyCapturable = True
             break
-    if not legallyCapturable: return True
-
+    if not legallyCapturable: 
+        if debug: print("No legal captures of it so it's defended")
+        return True
+    
     # calculate if piece was just equally or favourably traded
     move = board.pop()
     previousPiece = board.piece_at(square)
-    wasExchanged = previousPiece != None and pieceValues[previousPiece.piece_type] >= pieceValues[piece.piece_type]
+    wasExchanged = (
+        previousPiece != None 
+        and pieceValues[previousPiece.piece_type] >= pieceValues[piece.piece_type] 
+        and previousPiece.color != piece.color
+    )
     board.push(move)
+    if debug: print("Was the piece just exchanged equally or favourably: " + str(wasExchanged))
 
     # even with more defense than attack, if piece is being attacked by piece of lower value, it is undefended
     # if it's being attacked by a king, only give undefended if there are no defenders
@@ -54,4 +66,5 @@ def is_piece_safe(board: chess.Board, square: chess.Square) -> bool:
     if len(attackers) > len(defenders) and not wasExchanged:
         return False
     else:
+        if debug: print("There are either less attackers than defenders or it was just traded off")
         return True
